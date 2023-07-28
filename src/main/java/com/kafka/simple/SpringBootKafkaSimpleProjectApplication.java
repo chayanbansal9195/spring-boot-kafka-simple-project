2f8925244d22b89dd27da49918d2816a65d830ce
chayanbansal9195 <chayanbansal1999@gmail.com>
2023-07-28 19:25:43 +0530

    git initial setup

---
+++ src/main/java/com/kafka/simple/SpringBootKafkaSimpleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaSimpleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaSimpleProjectApplication.class, args);
+	}
+
+}
